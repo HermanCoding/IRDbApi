05a5ec340ab449f748f0cdd4e0e03748cb90ed29
HermaN <d.herman85@gmail.com>
2023-10-17 11:37:24 +0200

    Before Db generation

---
+++ IRDbApi/Models/MovieModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IRDbApi.Models
+{
+	public class MovieModel
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Movie title is required.")]
+		public required string Title { get; set; }
+		public string? Director { get; set; }
+		public int Year { get; set; }
+		public string? Genre { get; set; }
+		public int Duration { get; set; }
+		public double Rating { get; set; }
+	}
+}
